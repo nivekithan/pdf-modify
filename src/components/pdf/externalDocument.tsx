c3f4f1c803ee8cd3485d3932e55447d33d6a1e2e
Nivekithan S <nivekithan363@gmail.com>
2021-06-27 11:16:25 +0530

    Add Loader in ExternalDocument

--- src/components/pdf/externalDocument.tsx
+++ src/components/pdf/externalDocument.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Document } from "react-pdf";
 import { usePdfFile } from "~context/pdfFileProvider";
 import { useAppSelector } from "~hooks/store";
+import Spinner from "react-loader-spinner";
 
 type ExternalDocumentProps = {
   renderIndex: number;
@@ -13,5 +14,23 @@ export const ExternalDocument = ({ renderIndex, children }: ExternalDocumentProp
 
   const srcUrl = useAppSelector((state) => state.files.pdf[fileIndex].srcUrl[renderIndex]);
 
-  return <>{srcUrl ? <Document file={srcUrl}>{children}</Document> : children}</>;
+  return (
+    <>
+      {srcUrl ? (
+        <Document file={srcUrl} loading={<Loader />}>
+          {children}
+        </Document>
+      ) : (
+        children
+      )}
+    </>
+  );
+};
+
+const Loader = () => {
+  return (
+    <div className="h-[200px] w-[100px] grid place-items-center">
+      <Spinner type="Oval" height="42px" width="42px" />
+    </div>
+  );
 };
